024f2e131198b93567c5d0851d0236214b1774ae
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2021-10-30 20:19:24 +0600

    navbar[WIP]

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Home from './Pages/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>HELLO MEDICREW..!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light py-3">
+                <div className="container">
+                    <Link className="navbar-brand" to="/">
+                        <h2>Medicrew</h2>
+                    </Link>
+                    <button className="navbar-toggler" type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link active" to="/home">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/appointment">Make Appointment</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/dashboard">Dashboard</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/service">Service</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/review">Review</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/contact">Contact Us</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
