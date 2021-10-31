e22851e881e65e098be48373440ed1ab4dbf5084
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2021-10-31 22:52:15 +0600

    Banner section done

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "./Banner.css";
+import banner from "../../Images/banner/banner-img.png";
+const Banner = () => {
+    return (
+        <div className="banner-section">
+            <div className="container">
+                <div style={{ height: '90vh' }} className="row align-items-center">
+                    <div className="col-md-5">
+                        <h1>
+                            Doctor's Chamber  <br /> on Video Call
+                        </h1>
+                        <p className="my-4">Book video call appointments with the bangladesies most qualified specialist doctors in their private chambers.</p>
+                        <button className="btn btn-style appointment-btn">Make Appointment</button>
+                    </div>
+                    <div className="col-md-7 banner-img">
+                        <img src={banner} alt="banner-img" className="img-fluid" />
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default Banner;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light py-3">
                 <div className="container">
                     <Link className="navbar-brand" to="/">
-                        <h2>Medicrew</h2>
+                        <h1 style={{ color: "#1f2d53" }}>Medicrew</h1>
                     </Link>
                     <button className="navbar-toggler" type="button"
                         data-toggle="collapse"
@@ -18,7 +18,7 @@ const Navbar = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mt-2 mt-lg-0">
+                        <ul className="navbar-nav ms-auto mt-lg-0">
                             <li className="nav-item">
                                 <Link className="nav-link active" to="/home">Home</Link>
                             </li>
