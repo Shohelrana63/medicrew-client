d2d6e6df3d5dbd36aa7925926e31971ba1b69015
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2022-01-01 16:55:22 +0600

    add doctor form modified

--- src/Pages/Dashboard/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor.js
@@ -1,19 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState,useRef } from 'react';
 import { useEffect } from 'react';
 import Sidebar from '../../Components/Dashboard/Sidebar';
+import { Form } from 'react-bootstrap';
 
 const AddDoctor = () => {
 	useEffect(() => {
 		window.scrollTo(0, 0);
     }, []);
 	
-    const [ info, setInfo ] = useState({});
+    const [ info, setInfo ] = useState({
+		success: '',
+		error: ''
+	});
 	const [ file, setFile ] = useState(null);
 
 	const handleBlur = (e) => {
 		const newInfo = { ...info };
 		newInfo[e.target.name] = e.target.value;
 		setInfo(newInfo);
+	e.preventDefault();
 	};
 
 	const handleFileChange = (e) => {
@@ -21,8 +26,11 @@ const AddDoctor = () => {
 		setFile(newFile);
 	};
 
-	const handleSubmit = () => {
-		const formData = new FormData();
+	const formRef = useRef(null);
+
+	const handleReviewSubmit = (event) => {
+		event.preventDefault();
+			const formData = new FormData();
 		console.log(info);
 		formData.append('file', file);
 		formData.append('id', Math.round(Math.random() * 100 + 15));
@@ -33,18 +41,28 @@ const AddDoctor = () => {
 		formData.append('department', info.department);
 		formData.append('hospital', info.hospital);
 		formData.append('img', info.img);
-
-		fetch('http://localhost:8000/addADoctor', {
+			fetch('http://localhost:8000/addADoctor', {
 			method: 'POST',
 			body: formData
 		})
-			.then((response) => response.json())
+			.then((result) => {
+				const updateInfo = { ...info };
+				updateInfo.success = 'Doctor Added Successfully';
+                console.log(updateInfo.success)
+				updateInfo.error = '';
+				setInfo(updateInfo);
+				result.json();
+			})
 			.then((data) => {
 				console.log(data);
 			})
-			.catch((error) => {
-				console.error(error);
+			.catch((err) => {
+				const errUpdate = { ...info};
+				errUpdate.success = '';
+				errUpdate.error = 'something wrong happened';
+				setInfo(errUpdate);
 			});
+		formRef.current.reset();
 	};
     return (
         <section className="container-fluid row" style={{ backgroundColor: '#F4FDFB' }}>
@@ -52,110 +70,112 @@ const AddDoctor = () => {
 			<div id="responsive-dashboard" className="col-md-10 p-4 doctorForm"
              style={{position:"absolute", right: 0, backgroundColor: '#F4FDFB' }}>
 				<h5 className="text-brand mb-4">Add a Doctor</h5>
-				<form onSubmit={handleSubmit}>
-					<div className="form-group col-6">
-						<label htmlFor="exampleInputEmail1">Category</label>
-						<input
-							onBlur={handleBlur}
-							type="text"
-							className="form-control"
-							name="category"
-							placeholder="Enter Category"
-							required={true}
-						/>
-					</div>
-					<div className="form-group col-6">
-						<label htmlFor="exampleInputPassword1">Name</label>
-						<input
-							onBlur={handleBlur}
-							type="text"
-							className="form-control"
-							name="name"
-							placeholder="Enter Your Name"
-							required={true}
-						/>
-					</div>
-					<div className="form-group col-6">
-						<label htmlFor="exampleInputPassword1">Education</label>
-						<textarea
-							onBlur={handleBlur}
-							type="text"
-							className="form-control"
-							name="education"
-							placeholder="Enter Your Education"
-							required={true}
-						/>
-					</div>
-					<div className="form-group col-6">
-						<label htmlFor="exampleInputPassword1">Designation</label>
-						<input
-							onBlur={handleBlur}
-							type="text"
-							className="form-control"
-							name="designation"
-							placeholder="Enter Your Designation"
-							required={true}
-						/>
-					</div>
-					<div className="form-group col-6">
-						<label htmlFor="exampleInputPassword1">Department</label>
-						<input
-							onBlur={handleBlur}
-							type="text"
-							className="form-control"
-							name="department"
-							placeholder="Enter Your Department"
-							required={true}
-						/>
-					</div>
-					<div className="form-group col-6">
-						<label htmlFor="exampleInputPassword1">Hospital</label>
-						<input
-							onBlur={handleBlur}
-							type="text"
-							className="form-control"
-							name="hospital"
-							placeholder="Enter Your Hospital"
-							required={true}
-						/>
-					</div>
-					<div className="form-group col-6 pt-2">
-						<label htmlFor="exampleInputPassword1">
-							Upload a image in
-							<a
-								href="https://imgur.com/"
-								rel="noopener noreferrer"
-								target="_blank"
-								className="text-black px-2"
-							>
-								<strong>imgur</strong>
-							</a>
-							& Paste the image link here
-						</label>
-						<input
-							onBlur={handleBlur}
-							type="text"
-							className="form-control"
-							name="img"
-							placeholder="Upload Picture in imgur and paste the img link here"
-							required={true}
-						/>
-					</div>
-					<div className="form-group col-6">
-						<label htmlFor="exampleInputPassword1">Upload a image</label>
-						<input
-							onChange={handleFileChange}
-							type="file"
-							className="form-control"
-							id="exampleInputPassword1"
-                            placeholder="Picture"
-                            required={true}
-						/>
-					</div>
-					<button type="submit" className="btn btn-primary mt-2 ml-3">
-						Submit
-					</button>
-				</form>
+				<div className="">
+					{info.success && <p className="text-success font-weight-bold">{info.success}</p>}
+					{info.error && <p className="text-danger">{info.error}</p>}
+
+					<Form onSubmit={handleReviewSubmit} ref={formRef}>
+						<Form.Group className="col-6" controlId="formBasicText">
+							<Form.Label>Category</Form.Label>
+								<Form.Control
+									onBlur={handleBlur}
+									type="text"
+									className="form-control"
+									name="category"
+									placeholder="Enter Category"
+									required={true}
+							/>
+						</Form.Group>
+						<Form.Group className="col-6" controlId="formBasicText">
+							<Form.Label>Name</Form.Label>
+								<Form.Control
+									onBlur={handleBlur}
+									type="text"
+									className="form-control"
+									name="name"
+									placeholder="Enter Your Name"
+									required={true}
+								/>
+						</Form.Group>
+						<Form.Group className="col-6" controlId="formBasicText">
+							<Form.Label>Education</Form.Label>
+								<Form.Control
+									onBlur={handleBlur}
+									type="text"
+									className="form-control"
+									name="education"
+									placeholder="Enter Your Education"
+									required={true}
+								/>
+							</Form.Group>
+						<Form.Group className="col-6" controlId="formBasicText">
+							<Form.Label>Designation</Form.Label>
+								<Form.Control
+									onBlur={handleBlur}
+									type="text"
+									className="form-control"
+									name="designation"
+									placeholder="Enter Your Designation"
+									required={true}
+								/>
+						</Form.Group>
+						<Form.Group className="col-6" controlId="formBasicText">
+						<Form.Label>Department</Form.Label>
+							<Form.Control
+								onBlur={handleBlur}
+								type="text"
+								className="form-control"
+								name="department"
+								placeholder="Enter Your Department"
+								required={true}
+							/>
+						</Form.Group>
+						<Form.Group className="col-6" controlId="formBasicText">
+							<Form.Label>Hospital</Form.Label>
+								<Form.Control
+									onBlur={handleBlur}
+									type="text"
+									className="form-control"
+									name="hospital"
+									placeholder="Enter Your Hospital"
+									required={true}
+								/>
+						</Form.Group>
+						<Form.Group className="col-6" controlId="formBasicText">
+							<Form.Label>Upload a image in
+								<a href="https://imgur.com/"
+									rel="noopener noreferrer"
+									target="_blank"
+									className="text-black px-2"
+									>
+										<strong>imgur</strong>
+									</a> & Paste the image link here
+							</Form.Label>
+								<Form.Control
+									onBlur={handleBlur}
+									type="text"
+									className="form-control"
+									name="img"
+									placeholder="Upload Picture in imgur and paste the img link here"
+									required={true}
+								/>
+						</Form.Group>
+						<Form.Group className="col-6" controlId="formBasicText">
+							<Form.Label>Upload a image</Form.Label>
+								<Form.Control
+									onChange={handleFileChange}
+									type="file"
+									className="form-control"
+									id="exampleInputPassword1"
+									placeholder="Picture"
+									required={true}
+								/>
+						</Form.Group>
+						<button type="submit" className="btn btn-style mt-2 ml-3">
+							Submit
+						</button>
+					</Form>
+				</div>
 			</div>
 		</section>
     );
